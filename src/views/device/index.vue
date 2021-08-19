78d0b1e5a6260c3b132c2137b10f34ea3cfd9381
guanpeng <guanpeng@huimei.com>
2021-08-19 15:24:16 +0800

    add dashboard

--- src/views/device/index.vue
+++ src/views/device/index.vue
@@ -498,7 +498,7 @@ export default {
           sums[index] = values.reduce((prev, curr) => {
             const value = Number(curr)
             if (!isNaN(value)) {
-              return parseFloat((prev + ((index === 5 || index === 6) ? parseFloat((curr / 8 / 1024).toFixed(2)) : ((index === 7 || index === 8) ? parseFloat((curr / 8 / 1024 / 1024).toFixed(2)) : curr))).toFixed(2))
+              return parseFloat((prev + ((index === 6 || index === 7) ? parseFloat((curr / 8 / 1024).toFixed(2)) : ((index === 8 || index === 9) ? parseFloat((curr / 8 / 1024 / 1024).toFixed(2)) : curr))).toFixed(2))
             } else {
               return prev
             }
